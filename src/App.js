c635736102409ff751ecfaf9da3676ba0563942c
abel-tefera <abeltefera16@gmail.com>
2023-04-26 19:46:18 +0300

    Quotes added to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Calculator from './components/Calculator';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center text-2xl">Math Magicians</h1>
-      <Calculator />
+      <div className="flex flex-row justify-between">
+        <Quotes />
+        <Calculator />
+      </div>
     </div>
   );
 }
