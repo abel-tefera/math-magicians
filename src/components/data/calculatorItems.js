80739ad726a59987964f36b999c02084cd818ea9
abel-tefera <abeltefera16@gmail.com>
2023-04-27 10:26:59 +0300

    calculatorItems refactored

---
+++ src/components/data/calculatorItems.js
@@ -0,0 +1,128 @@
+const calculatorItems = [
+  [
+    {
+      bgColor: 'bg-gray-300',
+      val: 'AC',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '+/-',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '%',
+      stayFocused: true,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-orange-500',
+      val: '÷',
+      stayFocused: true,
+      width: 1,
+    },
+  ],
+  [
+    {
+      bgColor: 'bg-gray-300',
+      val: '7',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '8',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '9',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-orange-500',
+      val: '*',
+      stayFocused: true,
+      width: 1,
+    },
+  ],
+  [
+    {
+      bgColor: 'bg-gray-300',
+      val: '4',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '5',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '6',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-orange-500',
+      val: '-',
+      stayFocused: true,
+      width: 1,
+    },
+  ],
+  [
+    {
+      bgColor: 'bg-gray-300',
+      val: '1',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '2',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '3',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-orange-500',
+      val: '+',
+      stayFocused: true,
+      width: 1,
+    },
+  ],
+  [
+    {
+      bgColor: 'bg-gray-300',
+      val: '0',
+      stayFocused: false,
+      width: 2,
+    },
+    {
+      bgColor: 'bg-gray-300',
+      val: '.',
+      stayFocused: false,
+      width: 1,
+    },
+    {
+      bgColor: 'bg-orange-500',
+      val: '=',
+      stayFocused: false,
+      width: 1,
+    },
+  ],
+];
+
+export default calculatorItems;
