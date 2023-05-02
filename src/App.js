050656ed49b609517ddbf70e5d4099026e2940b6
abel-tefera <abeltefera16@gmail.com>
2023-05-02 12:33:46 +0300

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import {
+  Routes, Route, Link, BrowserRouter,
+} from 'react-router-dom';
+
 import './App.css';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import Welcome from './components/Welcome';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center text-2xl">Math Magicians</h1>
-      <div className="flex flex-row justify-around">
-        <Quotes />
-        <Calculator />
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Header />}>
+              <Route index element={<Welcome />} />
+              <Route path="calculator" element={<Calculator />} />
+              <Route path="quotes" element={<Quotes />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
