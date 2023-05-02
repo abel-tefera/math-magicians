3e7fddb0fe17be1a9aa0e4627da65d7c5fe5bd7b
abel-tefera <abeltefera16@gmail.com>
2023-05-02 12:33:23 +0300

    Created the welcome page

---
+++ src/components/Welcome.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Welcome = () => (
+  <div className="container ml-16">
+    <p className="text-2xl my-2">Welcome to Math Magicians</p>
+    <p className="text-lg">
+      Ullamco elit dolor consequat eu voluptate velit magna ad proident fugiat
+      voluptate do. Esse quis laborum voluptate magna mollit et fugiat excepteur
+      ipsum. Sunt irure qui ut veniam laborum non consectetur ea. Non eiusmod
+      consequat nostrud amet Lorem sit minim ipsum ea eu.
+      <br />
+      {' '}
+      <br />
+      Voluptate irure qui duis ex sit mollit id deserunt. Laborum exercitation
+      exercitation esse aute mollit labore labore voluptate velit consectetur do
+      fugiat. Laboris veniam amet cillum velit et voluptate irure excepteur duis
+      ipsum ex officia. Sint Lorem culpa do sit laboris anim voluptate nisi
+      minim.
+    </p>
+  </div>
+);
+
+export default Welcome;
