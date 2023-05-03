b6474be4e2944785c6559a0fb64dfa11b53deeb5
abel-tefera <abeltefera16@gmail.com>
2023-05-03 16:41:36 +0300

    Calculate tests set up

---
+++ src/_test_/calculate.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  render, screen, cleanup, fireEvent,
+} from '@testing-library/react';
+
+import calculate from '../logic/calculate';
+import Calculator from '../components/Calculator';
+
+describe('calculator UI testing', () => {
+
+});
+
+afterEach(cleanup);
+
+describe('testing calculations', () => {
+
+});
