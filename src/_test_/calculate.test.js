5b27bcb9d5d3060ce7f0fd0faebaa0c631741e59
abel-tefera <abeltefera16@gmail.com>
2023-05-03 16:42:02 +0300

    Test 1 for UI

--- src/_test_/calculate.test.js
+++ src/_test_/calculate.test.js
@@ -7,7 +7,18 @@ import calculate from '../logic/calculate';
 import Calculator from '../components/Calculator';
 
 describe('calculator UI testing', () => {
+  test('2 x 3', () => {
+    render(<Calculator />);
 
+    const answer = screen.getByTestId('in');
+
+    fireEvent.click(screen.getByText('6'));
+    fireEvent.click(screen.getByText('*'));
+    fireEvent.click(screen.getByText('3'));
+    fireEvent.click(screen.getByText('='));
+
+    expect(answer.innerHTML).toBe('18');
+  });
 });
 
 afterEach(cleanup);
