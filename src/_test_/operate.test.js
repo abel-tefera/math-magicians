65a302ad2410e882adf42812ea85b79a1f703d4e
citec-47 <mauricendonyi40@gmail.com>
2023-05-03 09:29:51 -0700

    Operation test

--- src/_test_/operate.test.js
+++ src/_test_/operate.test.js
@@ -1,28 +1,28 @@
 import operate from '../logic/operate';
 
 describe('Tests operating ', () => {
-  test('Test modulus 9 % 2 to get 4', () => {
-    const product = operate(9, 2, '%');
-    expect(product).toBe('4');
+  test('Test moduluses 7 % 3 to get 1', () => {
+    const product = operate(7, 2, '%');
+    expect(product).toBe('1');
   });
 
-  test('Test operate divid 15 / 3 to get 5', () => {
+  test('Test divided by 15 / 3 to get 5', () => {
     const product = operate(15, 3, '/');
     expect(product).toBe('5');
   });
 
-  test('Test multiplying 2 * 15 to get 30', () => {
-    const product = operate(2, 15, 'x');
-    expect(product).toBe('30');
+  test('Test multiply by 5 * 5 to get 25', () => {
+    const product = operate(5, 5, 'x');
+    expect(product).toBe('25');
   });
 
-  test('Test adding 1 + 4 to get 5', () => {
-    const product = operate(1, 4, '+');
+  test('Test addition 1 + 12 to get 13', () => {
+    const product = operate(1, 12, '+');
+    expect(product).toBe('13');
+  });
+
+  test('Test subtraction 9 - 4 to get 5', () => {
+    const product = operate(9, 4, '-');
     expect(product).toBe('5');
-  });
-
-  test('Test subtracting 8 - 4 to get 4', () => {
-    const product = operate(8, 4, '-');
-    expect(product).toBe('4');
   });
 });
