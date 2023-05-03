88eb4e10de3b045a73cff7c4c7c7342a3b2a64d1
abel-tefera <abeltefera16@gmail.com>
2023-05-03 16:40:40 +0300

    Box boreder

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="container box-border">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Header />}>
