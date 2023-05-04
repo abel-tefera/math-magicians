77fbd300af5eadfb90dbec28c16833ec4504e5fe
abel-tefera <abeltefera16@gmail.com>
2023-05-04 08:11:03 +0300

    reverted operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,27 +3,28 @@ import Big from 'big.js';
 export default function operate(numberOne, numberTwo, operation) {
   const one = Big(numberOne);
   const two = Big(numberTwo);
-
   if (operation === '+') {
     return one.plus(two).toString();
-  }
-  if (operation === '%') {
-    return one.mod(two).toString();
   }
   if (operation === '-') {
     return one.minus(two).toString();
   }
-
-  if (operation === 'x') {
+  if (operation === '*') {
     return one.times(two).toString();
   }
-
-  if (operation === '/') {
-    if (two === '0') {
-      return '0';
+  if (operation === '÷') {
+    try {
+      return one.div(two).toString();
+    } catch (err) {
+      return "Can't divide by 0.";
     }
-    return one.div(two).toString();
   }
-
+  if (operation === '%') {
+    try {
+      return one.mod(two).toString();
+    } catch (err) {
+      return "Can't find modulo as can't divide by 0.";
+    }
+  }
   throw Error(`Unknown operation '${operation}'`);
 }
